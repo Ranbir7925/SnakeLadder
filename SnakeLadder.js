21354388cfc434ccb5d126ca4dedfd13bd2fa5b1
Ranbir7925 <honeyranbir7925@gmail.com>
2020-11-01 20:33:39 +0530

    ADDED:Function to decide players next move

--- SnakeLadder.js
+++ SnakeLadder.js
@@ -1,8 +1,31 @@
+//Constant
+const LADDER = 1
+const SNAKE = 2
+
+//Variables
 var position = 0
 var diceValue = 0
+
+function getPosition(position,diceValue,gameOption){
+    switch(gameOption){
+        case LADDER:
+            position = position + diceValue;
+            return position
+        case SNAKE:
+            position = position - diceValue
+            return position
+        default:
+            return position            
+    }
+}
+
+
 function game(){
     diceValue = Math.floor(Math.random() * 6 ) + 1
+    gameOption = Math.floor(Math.random() * 3)
+    position = getPosition(position,diceValue,gameOption)
 }
 game()
 console.log(diceValue);
+console.log(gameOption);
 console.log(position);
