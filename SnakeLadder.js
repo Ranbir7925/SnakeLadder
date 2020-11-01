fc3783b454f51cf92223cd13386277982e597617
Ranbir7925 <honeyranbir7925@gmail.com>
2020-11-01 21:35:56 +0530

    REFACTORED:Breaked the code into function and added diceCount

--- SnakeLadder.js
+++ SnakeLadder.js
@@ -8,38 +8,50 @@ const MAX_POSITION = 100;
 var position = 0;
 var diceValue = 0;
 var gameOption =  0;
+var rollCount = 0;
 
 function getPosition(position,diceValue,gameOption){
     switch(gameOption){
         case LADDER:
-            if(position+diceValue <= MAX_POSITION){
-                position = position+diceValue;
-                return position;
-            }
-            else
-                return position;
+            return ladder(position,diceValue)
         case SNAKE:
-            if(position - diceValue < 0){
-                position = 0;
-                return position;
-            }
-            else{
-                position = position - diceValue;
-                return position;
-            }
+            return snake(position,diceValue)
         default:
             return position;       
     }
 }
 
+function ladder(position,diceValue){
+    if(position+diceValue <= MAX_POSITION){
+        position = position+diceValue;
+        return position;
+    }
+    else
+        return position;
+}
 
+function snake(position,diceValue){
+    if(position - diceValue < 0){
+        position = 0;
+        return position;
+    }
+    else{
+        position = position - diceValue;
+        return position;
+    }
+}
+
+function diceRoll(){
+    diceValue = Math.floor(Math.random() * 6 ) + 1
+    gameOption = Math.floor(Math.random() * 2) + 1
+    return getPosition(position,diceValue,gameOption)
+}
 function game(){
     position = MIN_POSITION;
     while(position < MAX_POSITION){
-        diceValue = Math.floor(Math.random() * 6 ) + 1
-        gameOption = Math.floor(Math.random() * 2) + 1
-        position = getPosition(position,diceValue,gameOption)
+        rollCount += 1;
+        position = diceRoll();
+        console.log(`No.of dice rolled ${rollCount} ---->>> Position of player is: ${position}`)
     }
 }
 game()
-console.log(`Current position of player is: ${position}`)
