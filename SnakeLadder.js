cd2f97586fa938b46e0b2bdd0d012e10180e3868
Ranbir7925 <honeyranbir7925@gmail.com>
2020-11-01 21:09:27 +0530

    ADDED:Player plays till winnign position is reached

--- SnakeLadder.js
+++ SnakeLadder.js
@@ -1,31 +1,44 @@
 //Constant
-const LADDER = 1
-const SNAKE = 2
+const LADDER = 1;
+const SNAKE = 2;
+const MIN_POSITION = 0;
+const MAX_POSITION = 100;
 
 //Variables
-var position = 0
-var diceValue = 0
+var position = 0;
+var diceValue = 0;
+var gameOption =  0;
 
 function getPosition(position,diceValue,gameOption){
     switch(gameOption){
         case LADDER:
             position = position + diceValue;
-            return position
+            return position;
         case SNAKE:
-            position = position - diceValue
-            return position
+            if(position - diceValue < 0){
+                position = 0;
+                return position;
+            }
+            else{
+                position = position - diceValue;
+                return position;
+            }
         default:
-            return position            
+            return position;       
     }
 }
 
 
 function game(){
-    diceValue = Math.floor(Math.random() * 6 ) + 1
-    gameOption = Math.floor(Math.random() * 3)
-    position = getPosition(position,diceValue,gameOption)
+    position = MIN_POSITION;
+    while(position < MAX_POSITION){
+        diceValue = Math.floor(Math.random() * 6 ) + 1
+        gameOption = Math.floor(Math.random() * 2) + 1
+        position = getPosition(position,diceValue,gameOption)
+    }
 }
 game()
 console.log(diceValue);
 console.log(gameOption);
 console.log(position);
+console.log(`Current position of player is: ${position}`)
